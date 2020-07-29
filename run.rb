5f4fe8a5a9a03e357c0c722b71b817ffbe416b8a
Eric Kim <hyeokjungkim@gmail.com>
2020-07-29 15:15:41 -0400

    first commit

---
+++ run.rb
@@ -0,0 +1,84 @@
+require 'pry'
+
+def instructors
+    [
+        {
+            name: "Annie Z",
+            role: "Coach",
+            siblings: 2,
+            owns_car: false
+        },
+        {
+            name: "Sylwia V",
+            role: "Lead",
+            siblings: 1,
+            owns_car: true
+        },
+        {
+            name: "Isabel L",
+            role: "Coach",
+            siblings: 0,
+            owns_car: true
+        },
+        {
+            name: "Eric K",
+            role: "Lead",
+            siblings: 4,
+            owns_car: false
+        }
+    ]
+end
+
+
+
+def all_names
+
+end
+
+
+
+def name_and_role
+
+end
+
+
+
+def all_coaches
+
+end
+
+
+
+def all_leads
+
+end
+
+
+
+def first_instructor_with_a_car
+
+end
+
+
+
+def instructors_with_more_than_1_sibling
+
+end
+
+
+
+def most_siblings
+
+end
+
+
+
+def total_siblings
+
+end
+
+
+
+
+binding.pry
+0
