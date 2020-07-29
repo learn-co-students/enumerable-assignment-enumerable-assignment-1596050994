375c7040e901d0ff1dbfe4f8aa2d29ddd7037d21
sbalgobin94 <sbalgobin94@gmail.com>
2020-07-29 16:48:34 -0400

    sam done

--- run.rb
+++ run.rb
@@ -32,53 +32,56 @@ end
 
 
 def all_names
-
+    instructors.collect do |instructor_hash|
+        instructor_hash[:name]
+    end
 end
-
 
 
 def name_and_role
-
+    instructors.collect do |instructor_hash|
+        {instructor_hash[:name] => instructor_hash[:role]}
+    end
 end
-
 
 
 def all_coaches
-
+    instructors.select do |instructor_hash|
+        instructor_hash[:role] == "Coach"
+    end
 end
-
 
 
 def all_leads
-
+    instructors.select do |instructor_hash|
+        instructor_hash[:role] == "Lead"
+    end
 end
-
 
 
 def first_instructor_with_a_car
-
+    instructors.find do |instructor_hash|
+        instructor_hash[:owns_car] == true
+    end
 end
-
-
 
 def instructors_with_more_than_1_sibling
-
+    instructors.select do |instructor_hash|
+        instructor_hash[:siblings] > 1
+    end
 end
-
 
 
 def most_siblings
-
+    instructors.max_by do |instructor_hash|
+        instructor_hash[:siblings]
+    end
 end
-
 
 
 def total_siblings
-
+    instructors.collect do |instructor_hash|
+        instructor_hash[:siblings]
+    end.sum
 end
 
-
-
-
-binding.pry
-0
