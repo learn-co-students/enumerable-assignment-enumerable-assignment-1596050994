0503095ec4e95178769792f7fe9b569b9a3378fa
Vina27 <Mtabali27@gmail.com>
2020-07-29 16:49:40 -0400

    Maysoon

--- run.rb
+++ run.rb
@@ -31,31 +31,56 @@ end
 
 
 
-def all_names
-
+def all_names(instructors)
+    instructors.map do |name, name_hash|
+        name_hash[:name]  #|teachers|
+        return[:name]
+    end 
 end
 
 
 
 def name_and_role
+   new_array = [ ]
+   insturctors.select do |names, roles|
+    if names == 
+     
+    end 
+   end 
+   h = { "a" => 100, "b" => 200, "c" => 300 }
+   h.select {|k,v| k > "a"}  #=> {"b" => 200, "c" => 300}
+   h.select {|k,v| v < 200}  #=> {"a" => 100}
 
 end
-
+new_array 
+end 
 
 
 def all_coaches
-
+    instructors.map do |coach, coach_hash|
+        coach_hash[:name]
+        return[:name]
+    end 
 end
 
 
 
 def all_leads
+    instructors.map do |lead, lead_hash|
+        lead_hash[:name]
+        return[:name]
+    end 
 
 end
 
 
 
 def first_instructor_with_a_car
+    instructor.find do |k, v|
+        if v == true
+            return[:name]
+        
+
 
 end
 
