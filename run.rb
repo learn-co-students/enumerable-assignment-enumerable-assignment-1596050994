3fc858ff6b14c09bb50e68d53a3113922b12b330
ChristyTropila <tropila04@gmail.com>
2020-07-29 16:48:07 -0400

    first assignment

--- run.rb
+++ run.rb
@@ -32,53 +32,112 @@ end
 
 
 def all_names
-
+ instructors.map do |names|
+   names[:name]
+  # binding.pry
+ end
 end
+
+all_names
+
 
 
 
 def name_and_role
+   instructors.map do |names|
+  # binding.pry
 
 end
+end
 
-
+name_and_role
 
 def all_coaches
 
+    instructors.find do |name|
+        if name[:role] === "Coach"
+         #   binding.pry
+         puts name
+        end
+    end
 end
+
+
+
+
+
 
 
 
 def all_leads
+    instructors.find do |name|
+        if name[:role] === "Lead"
+         #   binding.pry
+         puts name
+        end
+    end
 
 end
+
 
 
 
 def first_instructor_with_a_car
-
+   first_instructor= instructors.find do|name|
+        if name[:owns_car]==true
+            name[:owns_car]
+        end
+    end
 end
 
+#puts first_instructor_with_a_car
 
 
 def instructors_with_more_than_1_sibling
-
+  instructors.find do|name|
+    if name[:siblings] > 1
+        puts name
+    end
+end
 end
 
+#puts instructors_with_more_than_1_sibling
 
 
 def most_siblings
+sib_count=0
+  instructors.max_by do|name|
+
+   if name[:siblings] > sib_count
+   
+        sib_count = name[:siblings]
+    # binding.pry
+   end
+     name
+
+    end
 
 end
+
+
+
 
 
 
 def total_siblings
 
+    new_array=[]
+
+    instructors.map do |name|
+        new_array << name[:siblings]
+        #binding.pry
+    end
+  new_array.sum
 end
 
+puts total_siblings
 
 
 
-binding.pry
+#binding.pry
 0
