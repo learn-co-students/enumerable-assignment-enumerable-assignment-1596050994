112a55000cc2969aab139b7205fc20ae4a4a8389
Elronia <zar.catherine@gmail.com>
2020-07-29 16:50:16 -0400

    almost done

--- run.rb
+++ run.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def instructors
-    [
+   instructors = [
         {
             name: "Annie Z",
             role: "Coach",
@@ -32,53 +32,70 @@ end
 
 
 def all_names
-
+  instructors.map do |name|
+    instructors[:name]
+  end
 end
 
 
 
-def name_and_role
-
-end
+# def name_and_role
+#   instructors.map do |name|
+#     instructors[:name]
+#   end
+#   return {}
+# end
 
 
 
 def all_coaches
-
+  instructors.select do |coach|
+    instructors[:role] == "Coach"
+  end
 end
 
 
 
 def all_leads
-
+  instructors.select do |lead|
+    instructors[:role] == "Lead"
+  end
 end
 
 
 
 def first_instructor_with_a_car
-
+  instructors.find do |instructor|
+     instructors[:owns_car] = true
+  end
 end
 
 
 
 def instructors_with_more_than_1_sibling
-
+  instructors.select do |sibling|
+    instructors[:siblings] > 1
 end
 
 
 
 def most_siblings
-
+  instructors.max_by do |sibling|
+    instructors[:siblings]
+  end
 end
 
 
 
 def total_siblings
-
+  total = instructors.map do |sibling|
+    instructors[:siblings] 
+  end
+  total.sum
 end
 
 
 
 
-binding.pry
-0
+# binding.pry
+# 0
