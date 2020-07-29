15e94c9aac94d21f96736c4d3b2ec69b048464b0
fbado66 <fabdo66@gmail.com>
2020-07-29 16:48:42 -0400

    first challenge with Flatiron

--- run.rb
+++ run.rb
@@ -32,53 +32,106 @@ end
 
 
 def all_names
-
+    instructors.map do |instructors_hash_info|
+     instructors_hash_info[:name]
+    #  binding.pry
+    end 
 end
+
+# p all_names()
 
 
 
 def name_and_role
-
+    names = {}
+    instructors.map do |instructors_hash_info|
+        names = instructors_hash_info[:name], 
+        names = instructors_hash_info[:role]
+        # binding.pry
+    end 
 end
-
+#   p name_and_role()
 
 
 def all_coaches
+    instructors.select do |instructors_hash_info|
 
+        if instructors_hash_info[:role] == "Coach"
+            instructors_hash_info
+        end
+
+    end 
 end
-
+# p all_coaches
 
 
 def all_leads
+    instructors.select do |instructors_hash_info|
 
+        if instructors_hash_info[:role] == "Lead"
+            instructors_hash_info
+        end
+
+    end 
 end
+# p all_leads
 
 
 
 def first_instructor_with_a_car
+    instructors.find do |instructors_hash_info|
 
+        if instructors_hash_info[:owns_car] == true
+            instructors_hash_info
+        end
+
+    end 
 end
+# p first_instructor_with_a_car
 
 
 
 def instructors_with_more_than_1_sibling
+    instructors.select do |instructors_hash_info|
 
+        if instructors_hash_info[:siblings] > 1
+            instructors_hash_info
+        end
+
+    end     
 end
+# p instructors_with_more_than_1_sibling
 
 
 
 def most_siblings
+    siblings = 0
+    instructors.select do |instructors_hash_info|
 
+        if siblings < instructors_hash_info[:siblings] 
+            siblings = instructors_hash_info[:siblings]
+            instructors_hash_info.select do |last|
+                last[1]
+            end
+
+        end
+    end 
 end
-
+p most_siblings
 
 
 def total_siblings
-
+    siblings = 0
+    instructors.map do |instructors_hash_info|
+        siblings += instructors_hash_info[:siblings]
+    end
+    siblings
 end
 
+p total_siblings
 
 
 
-binding.pry
-0
+
+# binding.pry
+# 0
