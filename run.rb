6e0dd08704a98721e776d3e1e55ccf7fe4f9886e
nisayana <nisayana23@yandex.ru>
2020-07-29 16:50:18 -0400

    done

--- run.rb
+++ run.rb
@@ -32,49 +32,50 @@ end
 
 
 def all_names
-
+    instructors.map {|instructor| instructor[:name]}
+    # binding.pry
 end
 
 
 
 def name_and_role
-
+    instructors.reduce {|instructor| }
 end
 
 
 
 def all_coaches
-
+    instructors.select {|instructor| instructor[:role] == "Coach"}
 end
 
 
 
 def all_leads
-
+    instructors.select {|instructor| instructor[:role] == "Lead"}
 end
 
 
 
 def first_instructor_with_a_car
-
+    instructors.find {|instructor| instructor[:owns_car] == true}
 end
 
 
 
 def instructors_with_more_than_1_sibling
-
+    instructors.find_all {|instructor| instructor[:siblings] > 1}
 end
 
 
 
 def most_siblings
-
+    instructors.max_by {|instructor| instructor[:siblings]}
 end
 
 
 
 def total_siblings
-
+    instructors. 
 end
 
 
