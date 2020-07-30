9ee7889f5f111c5a35fd6ad0644b45583b6be1f6
mirsafirst47 <habib.triande@gmail.com>
2020-07-30 12:48:08 -0400

    First Assignment with Flatiron done

--- run.rb
+++ run.rb
@@ -32,61 +32,67 @@ end
 
 
 def all_names
-    instructors.map do|instructors_element| 
-        instructors_element[:name]
+    instructors.map do |instructors_info|
+        instructors_info[:name] 
     end
 end
 
 
 
 def name_and_role
-    name = 
-    instructors.map do |instructors_element|
+    instructors.map do |instructors_info|
+        { instructors_info[:name] => instructors_info[:role] } 
     end
 end
 
-binding.pry
 
 def all_coaches
-    instructors.find do |instructors_element|
-        instructors_element[:role] == "Coach"
+    instructors.select do |instructors_info|
+        instructors_info[:role] == "Coach"
     end
 end
 
 
 
 def all_leads
-    instructors.find do |instructors_element|
-        instructors_element[:role] == "Lead"
+    instructors.select do |instructors_info|
+        instructors_info[:role] == "Lead"
     end
 end
 
-binding.pry
 
 def first_instructor_with_a_car
-
+    instructors.find do |instructors_info|
+        instructors_info[:owns_car]# no need to put the value since it's eql to tru
+    end
 end
+
 
 
 
 def instructors_with_more_than_1_sibling
-
+    instructors.select do |instructors_info|
+        instructors_info[:siblings] > 1
+    end
+    
 end
-
 
 
 def most_siblings
-
+    instructors.max_by do |instructors_info|
+        instructors_info[:siblings]
+    end
 end
-
 
 
 def total_siblings
-
+    instructors.sum do |instructors_info|
+        instructors_info[:siblings]
+    end
 end
 
 
 
 
-binding.pry
-0
+
+#binding.pry
