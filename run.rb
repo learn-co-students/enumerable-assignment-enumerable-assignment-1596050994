5ff9fda828567c2789936e8b06ca45ceb0714d00
Jade Decordova <jadedandroid@Jades-MacBook-Air.local>
2020-07-31 13:16:43 -0400

    teacher-info-enumerated

--- run.rb
+++ run.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-def instructors
+def info
     [
         {
             name: "Annie Z",
@@ -32,22 +32,18 @@ end
 
 
 
-def all_names(instructor_info)
-    count = 0 
-    n_a = []
-    while count < instructor_info.length 
-     n_a << instructor_info[count][:name]
-     count += 1
+def all_names
+    info.map do |profile|
+        profile[:name]
     end
-    n_a
-    binding.pry
 
 end
 
 
 
-def name_and_role(instructor_info)
-    instructor_info.map{ |name, role|
+def name_and_role
+    info.map{ |profile|
+        {profile[:name]=> profile[:role]}
        # in
 }
 
@@ -56,41 +52,76 @@ end
 
 
 def all_coaches
+    info.select { |profile|
+        profile[:role] == "Coach"
+        }
 
 end
 
 
 
 def all_leads
-
+    info.select do |profile|
+        profile[:role] == "Lead"
+    
+    end
 end
 
 
 
 def first_instructor_with_a_car
+    binding.pry
+    info.find do |profile|
+        profile[:owns_car] == true
+    end
 
 end
 
 
 
 def instructors_with_more_than_1_sibling
+    info.select do |profile|
+        profile[:siblings] > 1
+    end
 
 end
 
 
 
 def most_siblings
+    binding.pry
+     info.max_by do |profile|
+        profile[:siblings]
+     end
 
 end
-
 
 
 def total_siblings
-
+    info.sum do |profile|
+        profile[:siblings]
+    end
 end
 
+# def total_siblings
+#   total_sibs =  info.map do|profile|
+#         profile[:siblings]    
+#     }
+#     total_sibs.sum
+# end
 
+# def new_def
+#     binding.profile
+#     info.eachdo |profile|
+#     p profile
 
+array_of_all_names = all_names()
+array_of_name_and_role = name_and_role()
+array_of_coaches = all_coaches()
 
-#binding.pry
+sylwia = first_instructor_with_a_car()
+annie_eric = instructors_with_more_than_1_sibling()
+eric = most_siblings()
+
+binding.pry
 0
