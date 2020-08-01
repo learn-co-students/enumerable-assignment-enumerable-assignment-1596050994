a4fd626cb1d0333a66261810912061b353afbe80
Vina27 <Mtabali27@gmail.com>
2020-08-01 16:45:47 -0400

    done

--- run.rb
+++ run.rb
@@ -30,80 +30,66 @@ def instructors
 end
 
 
-
-def all_names(instructors)
-    instructors.map do |name, name_hash|
-        name_hash[:name]  #|teachers|
-        return[:name]
+def all_names
+    instructors.map do |instructor_hash|
+    instructor_hash[:name]
     end 
-end
+end 
 
+array_of_all_names = all_names()
 
 
 def name_and_role
-   new_array = [ ]
-   insturctors.select do |names, roles|
-    if names == 
-     
-    end 
+   instructors.map do |instructor_hash| #(hash.zip)
+   {instructor_hash[:name] => instructor_hash[:role]}
    end 
-   h = { "a" => 100, "b" => 200, "c" => 300 }
-   h.select {|k,v| k > "a"}  #=> {"b" => 200, "c" => 300}
-   h.select {|k,v| v < 200}  #=> {"a" => 100}
+end 
+    
+array_of_name_and_role = name_and_role()
 
-end
-new_array 
+
+def all_coaches 
+    instructors.select do |instructor_hash|
+        instructor_hash[:role] == "Coach"
+    end 
 end 
 
 
-def all_coaches
-    instructors.map do |coach, coach_hash|
-        coach_hash[:name]
-        return[:name]
-    end 
-end
-
-
-
 def all_leads
-    instructors.map do |lead, lead_hash|
-        lead_hash[:name]
-        return[:name]
+    instructors.select do |instructor_hash|
+        instructor_hash[:role] == "Lead"
     end 
-
-end
-
-
+end 
+    
 
 def first_instructor_with_a_car
-    instructor.find do |k, v|
-        if v == true
-            return[:name]
-        
-
-
-end
-
-
+    instructors.find do |instructor_hash|
+        instructor_hash[:owns_car] == true 
+    end 
+end 
 
 def instructors_with_more_than_1_sibling
-
+    instructors.select do |instructor_hash|
+        instructor_hash[:siblings] > 1 
+    end 
 end
-
 
 
 def most_siblings
-
-end
-
-
+    instructors.max_by do |instructor_hash|
+        instructor_hash[:siblings]
+    end 
+end 
 
 def total_siblings
-
+    instructors.sum do |instructor_hash|
+        instructor_hash[:siblings]
+    end 
 end
 
-
-
-
+array_of_coaches = all_coaches()
+sylwia = first_instructor_with_a_car()
+annie_eric = instructors_with_more_than_1_sibling()
+eric = most_siblings()
 binding.pry
 0
